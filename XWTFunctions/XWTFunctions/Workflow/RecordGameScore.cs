a3fe85cdf296fbf6e5d90211fd16d4a1c83123f4
Phil Pursglove <philpursglove@users.noreply.github.com>
2022-08-23 22:14:25 +0100

    Outline workflow for recording score

---
+++ XWTFunctions/XWTFunctions/Workflow/RecordGameScore.cs
@@ -0,0 +1,64 @@
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.DurableTask;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Azure.WebJobs.Host;
+using Microsoft.Extensions.Logging;
+
+namespace XWTFunctions.Workflow
+{
+    public static class RecordGameScore
+    {
+        [FunctionName("RecordGameScore")]
+        public static async Task RunOrchestrator(
+            [OrchestrationTrigger] IDurableOrchestrationContext context)
+        {
+            await context.CallActivityAsync("RecordFirstPlayerResult", null);
+
+            using (var timeOutCTS = new CancellationTokenSource())
+            {
+                Task player2Approval = context.WaitForExternalEvent("Player2Approval");
+                Task toApproval = context.WaitForExternalEvent("TOApproval");
+
+                Task approval = Task.WhenAny(player2Approval, toApproval);
+
+                if (approval != null)
+                {
+                    timeOutCTS.Cancel();
+
+                    await context.CallActivityAsync("RecordFinalResult", null);
+                }
+            }
+        }
+
+        [FunctionName("RecordFirstPlayerResult")]
+        public static void RecordFirstPlayerResult()
+        {
+
+        }
+
+        [FunctionName("RecordFinalResult")]
+        public static void RecordFinalResult()
+        {
+
+        }
+
+
+        [FunctionName("RecordGameScore_HttpStart")]
+        public static async Task<HttpResponseMessage> HttpStart(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequestMessage req,
+            [DurableClient] IDurableOrchestrationClient starter,
+            ILogger log)
+        {
+            // Function input comes from the request content.
+            string instanceId = await starter.StartNewAsync("RecordGameScore", null);
+
+            log.LogInformation($"Started orchestration with ID = '{instanceId}'.");
+
+            return starter.CreateCheckStatusResponse(req, instanceId);
+        }
+    }
+}
