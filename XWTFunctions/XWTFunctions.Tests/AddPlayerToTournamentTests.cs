d8feb1d370d2c73dadfb68c5111988df6a11a111
Phil Pursglove <philpursglove@users.noreply.github.com>
2022-08-24 18:01:33 +0100

    First test for orchestration trigg3er

---
+++ XWTFunctions/XWTFunctions.Tests/AddPlayerToTournamentTests.cs
@@ -0,0 +1,65 @@
+using NUnit.Framework.Internal;
+using System.Net.Http.Headers;
+using System.Net;
+using System.Text;
+using Microsoft.Azure.WebJobs.Extensions.DurableTask;
+using NSubstitute;
+using XWTFunctions.Workflow;
+
+namespace XWTFunctions.Tests
+{
+    public class AddPlayerToTournamentTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public async Task HttpStart_returns_retryafter_header()
+        {
+            // Define constants
+            const string functionName = "AddPlayerToTournament";
+            const string instanceId = "7E467BDB-213F-407A-B86A-1954053D3C24";
+
+            // Mock TraceWriter
+            var loggerMock = Substitute.For<Microsoft.Extensions.Logging.ILogger>();
+
+            // Mock DurableOrchestrationClientBase
+            var clientMock = Substitute.For<IDurableClient>();
+
+            // Mock StartNewAsync method
+            clientMock.StartNewAsync(functionName).ReturnsForAnyArgs(instanceId);
+
+
+            // Mock CreateCheckStatusResponse method
+            clientMock.CreateCheckStatusResponse(new HttpRequestMessage() , instanceId, false).ReturnsForAnyArgs(new HttpResponseMessage
+            {
+                StatusCode = HttpStatusCode.OK,
+                Content = new StringContent(string.Empty),
+                Headers =
+                {
+                    RetryAfter = new RetryConditionHeaderValue(TimeSpan.FromSeconds(10))
+                }
+            });
+
+            // Call Orchestration trigger function
+            var result = await AddPlayerToTournament.HttpStart(
+                new HttpRequestMessage()
+                {
+                    Content = new StringContent("{}", Encoding.UTF8, "application/json"),
+                    RequestUri = new Uri("http://localhost:7071/orchestrators/AddPlayerToTournament"),
+                },
+                clientMock,
+                loggerMock);
+
+            // Validate that output is not null
+            Assert.NotNull(result.Headers.RetryAfter);
+
+            // Validate output's Retry-After header value
+            Assert.That(result.Headers.RetryAfter.Delta, Is.EqualTo(TimeSpan.FromSeconds(10)));
+        }
+
+        
+    }
+}
