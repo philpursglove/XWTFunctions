96281b6bbf201e56b24707fe56849d94ecf22fba
Phil Pursglove <philpursglove@users.noreply.github.com>
2022-09-01 17:01:20 +0100

    Extra info in the GameScore message

--- XWTFunctions/XWTFunctions/Messages/GameScoreMessage.cs
+++ XWTFunctions/XWTFunctions/Messages/GameScoreMessage.cs
@@ -4,5 +4,16 @@ namespace XWTFunctions.Messages;
 
 public class GameScoreMessage
 {
-    public Guid TournamentId { get; set; }
+    public Guid GameId { get; set; }
+
+    public int Player1Score { get; set; }
+    public int Player2Score { get; set; }
+
+    public int Turns { get; set; }
+
+    public bool Player1Concede { get; set; }
+    public bool Player2Concede { get; set; }
+
+    public bool Player1Drop { get; set; }
+    public bool Player2Drop { get; set; }
 }
