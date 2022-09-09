8456b00eb98dde5c0375b618ca1c0ffeb43516c4
Phil Pursglove <philpursglove@users.noreply.github.com>
2022-09-09 12:59:37 +0100

    Startup class for DI

---
+++ XWTFunctions/XWTFunctions/Startup.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: FunctionsStartup(typeof(XWTFunctions.Startup))]
+
+namespace XWTFunctions
+{
+
+    public class Startup : FunctionsStartup
+    {
+        public override void Configure(IFunctionsHostBuilder builder)
+        {
+            builder.Services.AddHttpClient();
+
+            //builder.Services.AddSingleton<IMyService>((s) => { return new MyService(); });
+
+            //builder.Services.AddSingleton<ILoggerProvider, MyLoggerProvider>();
+        }
+    }
+}
+
